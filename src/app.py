d97360e5d150880a37b8f348ce398442474b3e66
Andrey Sobolev <asobolev@ictp.it>
2014-03-19 23:47:48 +0100

    changed app.py to reflect changes in the code

--- src/app.py
+++ src/app.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 
 import sys
-#from read import Reader
+from read import RegCMReader, CRUReader
 from plot import Plotter
 
-class Reader(object):
-    pass
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
@@ -16,15 +14,20 @@ if __name__ == "__main__":
     obs_pattern = sys.argv[3]
     obs_nc_var = sys.argv[4]
 
-    r = Reader(pattern)
-    value = r.get_value(nc_var)
-    limits = value.get_limits()
+    r = RegCMReader(pattern)
+    value = r.get_value(nc_var).mean()
+    time_limits = value.get_limits('time')
+    crd_limits = value.get_latlonlimits()
 
-    obs_r = Reader(obs_pattern)
-    obs_value = r.get_value(obs_nc_var, limits = limits)
+    obs_r = CRUReader(obs_pattern)
+    obs_value = r.get_value(obs_nc_var, imposed_limits={'time': time_limits}, latlon_limits=crd_limits).mean()
+    if obs_nc_var == "TMP":
+        obs_value.to_K()
 
-    value.regrid(obs_value.grid)
+    value.regrid(obs_value.latlon)
     diff = abs(obs_value - value)
-    plt = Plotter()
-
-    plt.plot(diff.mean())
+    plt = Plotter(diff)
+    plt.plot()
+    plt.show()
+    plt.save('image', format='png')
+    plt.close()
