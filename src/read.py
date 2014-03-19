4ff5530bd896884d9a13995ab7784268f436c987
Andrey Sobolev <asobolev@ictp.it>
2014-03-19 23:27:27 +0100

    got to know latlon limits

--- src/read.py
+++ src/read.py
@@ -9,6 +9,16 @@ class Reader(object):
             self._files.append(f)
             self._ds.append(Dataset(f))
 
+    def _get_latlon_limits(self):
+        latlon_limits = {}
+        # in hope that all datasets have the same latitude and longitude points
+        ds = self._ds[0]
+        for crd, crd_name in self.crd_names.iteritems():
+            crd_value = ds.variables[crd_name][:]
+            latlon_limits[crd] = [np.min(crd_value), np.max(crd_value)]
+        return latlon_limits
+
+
 class RegCMReader(Reader):
     pass
 
