2217d94cf23712aa2f87f12e1e43ed1b28992e03
Andrey Sobolev <asobolev@ictp.it>
2014-03-19 23:38:13 +0100

    added the possibility to update the value (with data from another value)

--- src/value.py
+++ src/value.py
@@ -19,6 +19,52 @@ class Value(object):
         self.limits = limits
         self.latlon_limits = latlon_limits
 
+    def update(self, value):
+        # title update only if value is empty
+        if self.title is None:
+            self.title = value.title
+        else:
+            assert self.title == value.title
+        # the same goes with units
+        if self.units is None:
+            self.units = value.units
+        else:
+            assert self.units == value.units
+        # and with dim_names
+        if self.dim_names is None:
+            self.dim_names = value.dim_names
+        else:
+            assert self.dim_names == value.dim_names
+        # and with latlon
+        if self.latlon is None:
+            self.latlon = value.latlon
+        else:
+            assert np.all(self.latlon[0] == value.latlon[0])
+            assert np.all(self.latlon[1] == value.latlon[1])
+        # and with latlon_limits
+        if self.latlon_limits is None:
+            self.latlon_limits = value.latlon_limits
+        else:
+            assert self.latlon_limits == value.latlon_limits
+        # data(over time axis), dims(time) and limits(time) should be expanded
+        if self.data is None:
+            self.data = value.data
+        else:
+            # hope that time axis is 0 (can check for it in dims)
+            self.data = np.vstack((self.data, value.data))
+        # dims
+        if self.dims is None:
+            self.dims = value.dims
+        else:
+            # also hope that time axis is 0
+            self.dims[0] = np.hstack((self.dims[0], value.dims[0]))
+        # limits
+        if self.limits is None:
+            self.limits = value.limits
+        else:
+            # hope that time goes increasing
+            self.limits['time'] = [self.limits['time'][0], value.limits['time'][1]]
+
     def get_limits(self, name):
         return self.limits[name]
 
