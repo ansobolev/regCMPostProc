1fd483e30edc20defd6141e287612bc815d23753
Andrey Sobolev <asobolev@ictp.it>
2014-03-19 18:11:35 +0100

    plot changed according to changes in value class

--- src/plot.py
+++ src/plot.py
@@ -11,38 +11,46 @@ class Plotter(object):
 
     def __init__(self, value):
         self._value = value
-        self.lat, self.lon = value.grid
+        self.lat, self.lon = value.latlon
 
     def plot(self, coastlines=True,
                    countries=True,
                    places=True):
-        ax = plt.axes(projection=projection)
+        projection = ccrs.PlateCarree()
+        self.fig, self.ax = plt.subplots(subplot_kw={'projection': projection})
 
         if coastlines:
-            ax.coastlines('10m')
+            self.ax.coastlines('10m')
         if countries:
             countries = cfeature.NaturalEarthFeature(
                         scale='110m', category='cultural', name='admin_0_countries')
-            ax.add_feature(countries, color='r', alpha=0.1)
+            self.ax.add_feature(countries, color='r', alpha=0.1)
         if places:
             places = cfeature.NaturalEarthFeature(
                         scale='110m', category='cultural', name='populated_places')
-            ax.add_feature(places, color='b', hatch='o')
+            self.ax.add_feature(places, color='b', hatch='o')
 
-        ax.contourf(self.lon, self.lat, self.value, transform=ccrs.PlateCarree(),cmap='spectral')
+        cx = self.ax.contourf(self.lon, self.lat, self._value.data[0], transform=ccrs.PlateCarree(),cmap='spectral')
 
         # To mask out OCEAN or LAND
         #ax.add_feature(cfeature.OCEAN)
         #ax.add_feature(cfeature.LAND)
 
-        ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
+        self.ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
                         linewidth=1, color='blue', alpha=0.5, linestyle='-')
 
+        self.fig.colorbar(cx)
         times = self._value.limits['time']
 
-        plt.title(self._value.name + ' [' + self._value.unit + ']\n' +
-                        'mean between ' + str(times[0]) + ' and ' + str(times[1]) + '\n')
+        plt.title(self._value.title + ' [' + self._value.units + ']\n' +
+            'mean between ' + str(times[0]) + ' and ' + str(times[1]) + '\n')
+
+    def show(self):
         plt.show()
+
+    def save(self, filename, format):
+        plt.savefig(filename + '.' + format)
+
 
 if __name__ == "__main__":
     pass
