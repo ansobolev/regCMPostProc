e1d709e7b896c8954772a0b075c77e49e60a9b9e
Addisu <addisu@addisu-Satellite-C655.(none)>
2014-03-20 17:28:14 +0300

    This includes the method with montly average

--- src/value.py
+++ src/value.py
@@ -104,3 +104,42 @@ class Value(object):
 
     def to_K(self):
         self.data += 273.15
+
+    def m_average(names,data,times):
+        fdate = datetime.date(times[0])
+        edate = datetime.date(times[1])
+        ac = fdate.isocalendar()
+        bc = edate.isocalendar()
+        alist = []
+        blist = []
+        for i in ac:
+            alist.append(i)
+
+        for j in bc:
+            blist.append(j)
+
+        lcount = [0]
+        j = 0
+        count = 0
+        maverage = []
+        temp = []
+        while alist[0] <= blist[0]:
+                if alist[0] == blist[0] and alist[1] == blist[1] and alist[2] == blist[2]:
+                   break
+                while  alist[2] <= 11:
+                       d1 = datetime.date(alist[0], alist[2], alist[1])
+                       alist[2] += 1
+                       d2 = datetime.date(alist[0], alist[2], alist[1])
+                       diff = (d2 -d1).days
+                       for i in range(1,diff+1):
+                           lcount.append(i)
+                           count = count + diff
+                           chunk = lcount[0:diff]
+                temp = llcount.append(chunk)
+
+                alist[2] = 1
+                alist[0] += 1
+                m_mean = np.mean(self.data[temp], axis=0)
+                maverage.append(m_mean)
+    return maverage
+
